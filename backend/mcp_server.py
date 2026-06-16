9960e48b3f68caea593849e1e0d1f92a96d7dd47
syrine hentati <118487272+syrinehentati@users.noreply.github.com>
2026-06-16 19:57:47 +0100

    fix: using the correct version signature for mcp.run

--- backend/mcp_server.py
+++ backend/mcp_server.py
@@ -14,8 +14,4 @@ def generate_email(email_history, context, tone="warm", technical=False):
 
 
 if __name__ == "__main__":
-    mcp.run(
-        transport="sse",
-        host="0.0.0.0",                 # MUST
-        port=int(os.getenv("PORT", 8000))  # MUST
-    )
+    mcp.run(transport="sse")
